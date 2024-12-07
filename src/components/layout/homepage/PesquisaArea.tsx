4277360fb0e860a11b4305f88bc3c61c45260f87
carlosvale03 <111985773+carlosvale03@users.noreply.github.com>
2024-12-07 15:28:39 -0300

    style: implementar novo design do botão na area de pesquisa na Home

--- src/components/layout/homepage/PesquisaArea.tsx
+++ src/components/layout/homepage/PesquisaArea.tsx
@@ -1,13 +1,16 @@
 import Input from "./Input"
+import Button from "./Button"
 
 const PesquisaArea = () => {
     return (
         <div className="flex items-center justify-center">
-            <div className=" w-[80%] h-auto bg-[--white]">
-                <div>
-                    botoes
+            <div className=" w-[80%] h-auto bg-[--white] px-9 py-5 rounded-xl">
+                <div className="mb-6">
+                    <Button text="apartamento" flag='ap' />
+                    <Button text="casa" flag="cs" />
+                    <Button text="Quitinete" flag="qt" />
                 </div>
-                <div>
+                <div className="">
                     <Input text="teste de testo (mudar)" />
                 </div>
             </div>
